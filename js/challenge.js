b5dd66b6cd45be9ea03d22f9f76c782677194589
Jose <joseluis0528@gmail.com>
2023-04-17 23:06:57 -0500

    working with clearInterval

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,25 @@
 const getCounter = document.querySelector('#counter');
 let counter = 0;
-setInterval(() => {
+let setIntervalID = setInterval(() => {
     counter += 1;
-    getCounter.textContent = counter
+    getCounter.textContent = counter;
 }, 1000);
+
+const pauseBtn = document.querySelector('#pause');
+pauseBtn.addEventListener('click', () => {
+    clearInterval(setIntervalID);
+    document.querySelector('#minus').disabled = true;
+    document.querySelector('#plus').disabled = true;
+    document.querySelector('#heart').disabled = true;
+    pauseBtn.id = 'resume';
+    pauseBtn.textContent = 'resume';
+
+    // document.querySelector('#resume').addEventListener('click', () => {
+    //     setIntervalID
+    //     document.querySelector('#minus').disabled = false;
+    //     document.querySelector('#plus').disabled = false;
+    //     document.querySelector('#heart').disabled = false;
+    //     pauseBtn.id = ('#pause');
+    //     pauseBtn.textContent = 'pause'
+    // })
+})
